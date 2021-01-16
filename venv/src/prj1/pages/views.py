9c7627187abcb00757961d5ce81755bd1ec28a1a
Khophanmem24h\Admin <mxuanvan159@gmail.com>
2021-01-16 18:09:00 +0700

    1:44:22

--- venv/src/prj1/pages/views.py
+++ venv/src/prj1/pages/views.py
@@ -2,5 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Van Provip</h1>")
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>VanDepTrai</h1>")
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123,45243,32],
+        "my_html": "<h1>Hello World</h1>"
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
--- venv/src/prj1/prj1/urls.py
+++ venv/src/prj1/prj1/urls.py
@@ -16,16 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view, contact_view, about_view
+
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('about/', about_view),
+    path('contact/', contact_view),
     path('admin/', admin.site.urls),
 ]
 
 
-# myproject/urls.py
-from django.conf.urls import url, include
-from django.contrib import admin
-
-urlpatterns = [
-    url('^admin/', admin.site.urls),
-    # url(r'^posts/', include('posts.urls', namespace='posts')),
-]

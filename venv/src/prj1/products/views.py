e83e7ddf903482178fbff48e02422ffb7d96c96e
Khophanmem24h\Admin <mxuanvan159@gmail.com>
2021-01-19 17:19:33 +0700

    xong bai 22

--- venv/src/prj1/products/views.py
+++ venv/src/prj1/products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
+
+from .models import Product
 # Create your views here.
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is.valid():
+# 		form.save()
+#
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, "products/product_create.html", context)
+
+
+
+def product_detail_view(request):
+	obj = Product.objects.get(id=1)
+	# context = {
+	# 	'object': obj.title,
+	# 	'description': obj.description
+	# }
+
+	context = {
+		'object': obj
+	}
+	return render(request, "products/product_detail.html", context)

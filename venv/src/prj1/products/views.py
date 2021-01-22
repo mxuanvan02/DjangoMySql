ee44f784c378b80b6890c66e6fe29d1c4744808b
Khophanmem24h\Admin <mxuanvan159@gmail.com>
2021-01-22 18:37:08 +0700

    2:30:51

--- venv/src/prj1/products/views.py
+++ venv/src/prj1/products/views.py
@@ -17,12 +17,9 @@ from .models import Product
 
 
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-
+	my_form = RawProductForm()
 	context = {
-		'form': form
+		"form": my_form
 	}
 	return render(request, "products/product_create.html", context)
 
